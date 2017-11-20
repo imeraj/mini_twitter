71c8fe1f6968cf7efecbcf206a2bb05d1a04a3a5
Meraj <meraj.enigma@gmail.com>
2017-11-20 21:10:15 +0600

    Implemented advanced login (without tests)
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -30,7 +30,10 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not logged_in?
     assert_redirected_to root_url
+
+    delete logout_path
     follow_redirect!
+
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
